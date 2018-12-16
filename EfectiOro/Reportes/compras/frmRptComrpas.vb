c65a58c403570a24f987ef46e0ef9b0f4fe59175
Jose Miguel Romero Sevilla <drive.innovasoft@outlook.es>
2018-12-16 07:20:18 -0600

    ultimos ajustes

--- EfectiOro/Reportes/compras/frmRptComrpas.vb
+++ EfectiOro/Reportes/compras/frmRptComrpas.vb
@@ -254,17 +254,17 @@ Public Class frmRptComrpas
                     'ver reporte onzas                    
                     Try
                         Dim lisGeneral =
-                            (From c In ctx.Compras
-                                Join cli In ctx.Cliente On c.Codcliente Equals cli.Codcliente
-                                Join dc In ctx.Det_compra On c.Numcompra Equals dc.Numcompra
-                                Join tc In ctx.TipoCambio On c.Fecha Equals tc.Fecha
-                                Where
+(From c In ctx.Compras
+ Join cli In ctx.Cliente On c.Codcliente Equals cli.Codcliente
+ Join dc In ctx.Det_compra On c.Numcompra Equals dc.Numcompra
+ Join tc In ctx.TipoCambio On c.Fecha Equals tc.Fecha
+ Where
                                   dc.Importe > Decimal.Zero And
                                   dc.Fecha >= txtDesdeGen.Value And dc.Fecha <= txtHastaGen.Value And
                                   c.Codestado >= 1 And c.Codestado <= 3 And c.Codagencia = dc.Codagencia
-                                Order By
+ Order By
                                 dc.Fecha Descending
-                                Select
+ Select
                                   dc.Codagencia,
                                   dc.Numcompra,
                                   c.Codcliente,
@@ -278,12 +278,8 @@ Public Class frmRptComrpas
                                   dc.Fecha,
                                   tc.Tipocambio1,
                                   tc.Precio_oro,
-                                  Margen = CType((1 - (((CDbl((dc.Importe / CDec((If(
-                                 dc.Peso = CDec(0), CDec(1), CDec(dc.Peso))))) / CDec((If(
-                                 tc.Tipocambio1 = CDec(0), CDec(1), CDec(tc.Tipocambio1))))) * 31.1035 * 24) / CDbl(CDec(dc.Kilate))) / CDbl(CDec((If(
-                                 tc.Precio_oro = CDec(0), CDec(1), CDec(tc.Precio_oro))))))), Double?),
-                                  PrecioKilate = CType((CDbl(dc.Importe / CDec((If(
-                                 dc.Peso = CDec(0), CDec(1), CDec(dc.Peso))))) / CDbl(dc.Kilate)), Double?)
+                                  Margen = CType((1 - (Convert.ToDecimal(dc.Importe / Convert.ToDecimal(If(dc.Peso = Decimal.Zero, Decimal.One, Convert.ToDecimal(dc.Peso))) / If(tc.Tipocambio1 = Decimal.Zero, Decimal.One, tc.Tipocambio1)) * 31.1035 * 24 / CDec(dc.Kilate) / If(tc.Precio_oro = Decimal.Zero, Decimal.One, tc.Precio_oro))), Decimal),
+                                  PrecioKilate = CType((CDbl(dc.Importe / CDec((If(dc.Peso = Decimal.Zero, CDec(1), CDec(dc.Peso))))) / CDbl(dc.Kilate)), Decimal)
                                 ).ToList()
                         If chkFiltrarAgencia.Checked Then
                             lisGeneral = (From li In lisGeneral Where li.Codagencia = _agencia Select li).ToList
